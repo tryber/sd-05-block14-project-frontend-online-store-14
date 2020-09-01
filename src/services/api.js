52057e1a1779b1d677ffa6f0dedeaf3491046e04
fomes <contato.felipeluna@gmail.com>
2020-09-01 18:07:45 -0300

    api dev

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // implement here
+  fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => response.json())
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  let url;
+  if (categoryId && query) url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  else if (query) url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  else url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const result = fetch(url).then((response) => response.json());
+  return result;
 }
